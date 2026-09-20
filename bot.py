b8f502620b75ca3f0a1361922bebb1b83ec02ad5
oxuser11 <oxuser11@gmail.com>
2026-09-20 20:09:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,22 @@
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot running"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    Thread(target=run).start()
+
+keep_alive()
+
 import logging
 import asyncio
 from telegram import (
